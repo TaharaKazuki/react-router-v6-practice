ba9a561ac815f09df2f1d50df04c7b06db3de70b
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-01-25 22:48:26 +0900

    update

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Post = () => {
-  return <div>Post</div>
+  const { postId } = useParams<{ postId: string }>()
+  const [post, setPost] = useState<{ id: string; title: string; body: string } | null>(null)
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      const data = await res.json()
+      setPost(data)
+    }
+    fetchPost()
+  }, [postId])
+
+  return (
+    <>
+      <h2>Single Post</h2>
+      <div>
+        <p>ID: {post?.id}</p>
+        <p>タイトル：{post?.title}</p>
+        <p>内容：{post?.body}</p>
+      </div>
+    </>
+  )
 }
 
 export default Post
--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 const Posts = () => {
+  const [posts, setPosts] = useState<Array<{ id: string; title: string }>>([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const data = await res.json()
+      setPosts(data)
+    }
+    fetchPosts()
+  }, [])
+
   return (
-    <div>
-      Posts
+    <>
+      <h2>Posts</h2>
       <Outlet />
-    </div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={`/posts/${post.id}`}>
+              {post.id}:{post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
