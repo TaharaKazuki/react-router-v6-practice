a7de6c4e2adef707250ddd5be8baebf7984dcdc8
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-01-25 21:06:55 +0900

    update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
-  return <div>About</div>
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <h2>About</h2>
+      <button onClick={() => navigate('/contact')}>Contact</button>
+    </>
+  )
 }
 
 export default About
